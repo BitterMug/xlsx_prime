210a506cdee5331730c93b40f6e83bd2df61473f
BitterMug <veselyt715@gmail.com>
2021-01-31 23:51:57 +0100

    JAR file size reduction

--- src/main/java/org/bitter/xlsx_primenum/Main.java
+++ src/main/java/org/bitter/xlsx_primenum/Main.java
@@ -1,9 +1,10 @@
 package org.bitter.xlsx_primenum;
 
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
